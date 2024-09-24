1548b7e4888f10bfde56bf2ed7c23f39f7b6b8b2
daviline14 <156429877+daviline14@users.noreply.github.com>
2024-09-25 00:06:26 +0200

    Create league function

--- app/Http/Controllers/LeaguesController.php
+++ app/Http/Controllers/LeaguesController.php
@@ -8,9 +8,15 @@ use App\Models\League;
 class LeaguesController extends Controller
 {
     public function show(){
-        return view('leagues');
+        return view('league.leagues');
     }
-    public function create_league(){
+    public function create_league(Request $request){
+        League::create([
+            'name'=>$request->name,
+            'category'=>$request->category,
+            'max_racers'=>$request->max_racers
+        ]);
 
+        return redirect('/leagues')->with('success', 'Has creado la liga correctamente');
     }
 }
